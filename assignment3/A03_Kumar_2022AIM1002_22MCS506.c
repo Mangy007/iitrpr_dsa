87c1b1f99ba04afeed04e4811e60a7b41383fc2f
Kumar Mangalam <manguatwork@gmail.com>
2022-08-31 12:30:26 +0530

    bst insertion and inorder traversal done

---
+++ assignment3/A03_Kumar_2022AIM1002_22MCS506.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int value;
+    struct Node * left;
+    struct Node * right;
+} Node;
+
+Node * root = NULL;
+
+void printInorder(Node * node) {
+    if(node==NULL) return;
+    printInorder(node->left);
+    printf("%d->", node->value);
+    printInorder(node->right);
+}
+
+void printPostorder(Node * node) {
+    if(node==NULL) return;
+    printPostorder(node->left);
+    printPostorder(node->right);
+    printf("%d->", node->value);
+}
+
+void insertIntoBST(int val) {
+    if(root==NULL) {
+        root = (Node *) malloc(sizeof(Node));
+        root->value = val;
+        root->left = NULL;
+        root->right = NULL;
+    }
+    else {
+        Node * node = root;
+        while(node != NULL) {
+            if(node->value == val) return;
+            else if(node->value > val) {
+                if(node->left == NULL) {
+                    Node * newNode = (Node *) malloc(sizeof(Node));
+                    newNode->value = val;
+                    newNode->left = NULL;
+                    newNode->right = NULL;
+                    node->left = newNode;
+                }
+                node = node->left;
+            }
+            else {
+                if(node->right == NULL) {
+                    Node * newNode = (Node *) malloc(sizeof(Node));
+                    newNode->value = val;
+                    newNode->left = NULL;
+                    newNode->right = NULL;
+                    node->right = newNode;
+                }
+                node = node->right;
+            }
+        }
+    }
+}
+
+int main() {
+
+    insertIntoBST(4);
+    insertIntoBST(5);
+    insertIntoBST(3);
+    insertIntoBST(6);
+    insertIntoBST(7);
+    insertIntoBST(1);
+    Node * node = root;
+    printInorder(node);
+    printf("\n");
+    printPostorder(node);
+    
+    return 0;
+}
