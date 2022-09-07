9f20092d3e0d596c526efe3a2e23b30f63e98f4d
Kumar Mangalam <manguatwork@gmail.com>
2022-09-07 12:46:37 +0530

    postorder iterative done

--- assignment3/A03_Kumar_2022AIM1002_22MCS506.c
+++ assignment3/A03_Kumar_2022AIM1002_22MCS506.c
@@ -9,32 +9,65 @@ typedef struct Node {
 
 Node * root = NULL;
 
+void printPreorder(Node * node);
 void printInorder(Node * node);
 void printPostorder(Node * node);
 void insertIntoBST(int val);
+int numberofNodes(Node * node);
 int numberofLeaves(Node * node);
+void isNodePresent(int val);
 
 
 int main() {
 
     insertIntoBST(4);
     insertIntoBST(5);
-    insertIntoBST(3);
+    insertIntoBST(2);
     insertIntoBST(6);
     insertIntoBST(7);
     insertIntoBST(1);
+    insertIntoBST(3);
     Node * node = root;
+    printf("\n Preorder: ");
+    printf("[");
+    printPreorder(node);
+    printf("]");
+    printf("\n Inorder: ");
+    printf("[");
     printInorder(node);
-    printf("\n");
+    printf("]");
+    printf("\n Postorder: ");
+    printf("[");
     printPostorder(node);
+    printf("]");
+    printf("\n Number of nodes: %d", numberofNodes(node));
+    printf("\n Number of leaves: %d", numberofLeaves(node));
+    printf("\n");
+    isNodePresent(3);
+    isNodePresent(10);
+
     
     return 0;
+}
+
+void printPreorder(Node * node) {
+    // non recursive approach
+    int N = numberofNodes(node);
+    Node * stack[N];
+    int stackPointer = -1;
+    stack[++stackPointer] = node;
+    while(stackPointer>=0) {
+        Node * tempNode = stack[stackPointer--];
+        printf("%d ", tempNode->value);
+        if(tempNode->right != NULL) stack[++stackPointer] = tempNode->right;
+        if(tempNode->left != NULL) stack[++stackPointer] = tempNode->left;
+    }
 }
 
 void printInorder(Node * node) {
     if(node==NULL) return;
     printInorder(node->left);
-    printf("%d->", node->value);
+    printf("%d ", node->value);
     printInorder(node->right);
 }
 
@@ -42,7 +75,7 @@ void printPostorder(Node * node) {
     if(node==NULL) return;
     printPostorder(node->left);
     printPostorder(node->right);
-    printf("%d->", node->value);
+    printf("%d ", node->value);
 }
 
 void insertIntoBST(int val) {
@@ -80,8 +113,31 @@ void insertIntoBST(int val) {
     }
 }
 
-int numberofLeaves(Node * node) {
+int numberofNodes(Node * node) {
     if(node==NULL) return 0;
     if(node->left == NULL && node->right == NULL) return 1;
     return 1 + numberofLeaves(node->left) + numberofLeaves(node->right);
+}
+
+int numberofLeaves(Node * node) {
+    if(node==NULL) return 0;
+    if(node->left == NULL && node->right == NULL) return 1;
+    return numberofLeaves(node->left) + numberofLeaves(node->right);
+}
+
+void isNodePresent(int val) {
+    Node * node = root;
+    if(node == NULL || val <= 0) {
+        printf("NO\n");
+        return;
+    }
+    while(node != NULL) {
+        if(node->value==val) {
+            printf("YES\n");
+            return;
+        }
+        else if(node->value>val) node = node->left;
+        else node = node->right;
+    }
+    printf("NO\n");
 }
