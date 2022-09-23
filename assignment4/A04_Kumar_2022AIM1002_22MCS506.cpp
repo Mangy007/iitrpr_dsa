eea62cbb1b636e6f1241b48e037a3655a936f64a
mangy007 <manguatwork@gmail.com>
2022-09-23 10:42:13 +0530

    graph representation done

--- assignment4/A04_Kumar_2022AIM1002_22MCS506.cpp
+++ assignment4/A04_Kumar_2022AIM1002_22MCS506.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+typedef struct Node {
+    int key;
+    int height;
+} Node;
+
 void performOperationBasedOnQueryType(int query);
 void findQuickestPathBasedOnM(vector<vector<int>> adjMat, int M, int sx, int sy, int vx, int vy);
 
@@ -21,32 +26,79 @@ int main() {
     return 0;
 }
 
-vector<vector<int>> getGraphRepresentationInAdjacencyMatrix(int N, int M) {
-    vector<vector<int>> adjMat;
+vector<vector<Node *>> getGraphRepresentation(int N, int M) {
+    vector<vector<Node *>> adjList;
+    Node * heights[N][N];
+    int buildingNumber = 1;
     for(int i=0;i<N;i++) {
-        vector<int> row;
-        for(int j=0;j<M;j++) {
-            int input;
-            cin >> input;
-            row.push_back(input);
+        for(int j=0;j<N;j++) {
+            int heightOfBuilding;
+            cin >> heightOfBuilding;
+            Node * temp = (Node *) malloc(sizeof(Node));
+            temp->key = buildingNumber++;
+            temp->height = heightOfBuilding;
+            heights[i][j] = temp;
         }
-        adjMat.push_back(row);
     }
-    return adjMat;
+    for(int i=0; i<N; i++) {
+        // adjList.push_back(i+1);
+        for(int j=0; j<N; j++) {
+            vector<Node *> nodes;
+            nodes.push_back(heights[i][j]);
+            if(M==2) {
+                if(j+1<N) nodes.push_back(heights[i][j+1]);
+                if(i+1<N) nodes.push_back(heights[i+1][j]);
+            }
+            else if(M==4) {
+                if(j-1>0) nodes.push_back(heights[i][j-1]);
+                if(i-1>0) nodes.push_back(heights[i-1][j]);
+                if(j+1<N) nodes.push_back(heights[i][j+1]);
+                if(i+1<N) nodes.push_back(heights[i][j+1]);
+            }
+            else if(M==6) {
+                if(j-1>0) nodes.push_back(heights[i][j-1]);
+                if(i-1>0) nodes.push_back(heights[i-1][j]);
+                if(j+1<N) nodes.push_back(heights[i][j+1]);
+                if(i+1<N) nodes.push_back(heights[i][j+1]);
+                if(i-1>0 && j+1<N) nodes.push_back(heights[i-1][j+1]);
+                if(i+1<N && j+1<N) nodes.push_back(heights[i+1][j+1]);
+            }
+            else if(M==8) {
+                if(j-1>0) nodes.push_back(heights[i][j-1]);
+                if(i-1>0) nodes.push_back(heights[i-1][j]);
+                if(j+1<N) nodes.push_back(heights[i][j+1]);
+                if(i+1<N) nodes.push_back(heights[i][j+1]);
+                if(i-1>0 && j+1<N) nodes.push_back(heights[i-1][j+1]);
+                if(i+1<N && j+1<N) nodes.push_back(heights[i+1][j+1]);
+                if(i-1>0 && j-1>0) nodes.push_back(heights[i-1][j-1]);
+                if(i+1<N && j-1>0) nodes.push_back(heights[i+1][j-1]);
+            }
+            else return adjList;
+            adjList.push_back(nodes);
+        }
+    }
+    return adjList;
 }
 
-//test function: later to be deleted
-void printGraph(vector<vector<int>> adjMat) {
-    for(auto i=adjMat.begin(); i!=adjMat.end(); i++) {
-        for(auto j=(*i).begin(); j!=(*i).end(); j++) {
-            cout << *j <<" ";
+//test function: might delete later
+void printGraph(vector<vector<Node *>> adjList) {
+
+    for(vector<Node *> node : adjList) {
+        for(Node * elem : node) {
+            cout << elem->key << "(" << elem->height << ") ->";
         }
         cout << endl;
     }
+    // for(auto i=adjList.begin(); i!=adjList.end(); i++) {
+    //     for(auto j=(*i).begin(); j!=(*i).end(); j++) {
+    //         cout << (*j)->key << "(" << (*j)->height << ") ->";
+    //     }
+    //     cout << endl;
+    // }
 }
 
-void findQuickestPathBasedOnM(vector<vector<int>> adjMat, int M, int sx, int sy, int vx, int vy) {
-    
+void findQuickestPath(vector<vector<Node *>> graph, int source, int dest) {
+
 }
 
 void performOperationBasedOnQueryType(int query) {
@@ -56,23 +108,25 @@ void performOperationBasedOnQueryType(int query) {
         // sortest path by time
         int N, M, sx, sy, vx, vy;
         cin >> N >> M >> sx >> sy >> vx >> vy;
-        vector<vector<int>> adjMat = getGraphRepresentationInAdjacencyMatrix(N, N);
-        printGraph(adjMat);
-        findQuickestPathBasedOnM(adjMat, M, sx, sy, vx, vy);
+        vector<vector<Node *>> adjList = getGraphRepresentation(N, M);
+        printGraph(adjList);
+        int source = (sx-1)*N + sy;
+        int destination = (vx-1)*N + vy;
+        findQuickestPath(adjList, source, destination);
         break;
     }
     case 2: {
         // bfs
         int N, d, sx, sy, vx, vy;
         cin >> N >> d >> sx >> sy >> vx >> vy;
-        vector<vector<int>> adjMat = getGraphRepresentationInAdjacencyMatrix(N, N);
+        // vector<vector<int>> adjMat = getGraphRepresentation(N, 4);
         break;
     }
     case 3: {
         // topological sort
         int N;
         cin >> N;
-        vector<vector<int>> adjMat = getGraphRepresentationInAdjacencyMatrix(N, N);
+        // vector<vector<int>> adjMat = getGraphRepresentation(N, 0);
         break;
     }
     default:
